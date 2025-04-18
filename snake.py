1512508bb5fa132425c5ff96f7b2498433acaca0
Karl Adrian <karladrian995@gmail.com>
2025-04-18 22:04:01 +0300

    create window sizes

---
+++ snake.py
@@ -0,0 +1,18 @@
+import pygame, sys, time, random
+
+speed = 15
+
+#window sizes
+
+frame_size_x = 720
+frame_size_y = 480
+
+
+check_errors = pygame.init()
+
+if(check_errors[1] > 0):
+    print("Error " + check_errors[1])
+else:
+    print("Game Started")
+
+#start game window
