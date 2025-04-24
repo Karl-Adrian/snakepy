c5ffaf58313b275fe59e4b2f9d33d6a78fb401d3
Karl Adrian <karladrian995@gmail.com>
2025-04-24 13:17:58 +0300

    finished game loop

--- snake.py
+++ snake.py
@@ -18,4 +18,24 @@ else:
 #start game window
 
 pygame.display.set_caption("Snake Game")
-game_window = pygame.display.set_mode((frame_size_x,frame_size_y))
+game_window = pygame.display.set_mode(frame_size_x,frame_size_y)
+
+#colors
+
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+
+fps_controller = pygame.time.Clock()
+
+#one snake square size
+square_size = 20
+
+def init_vars():
+    global head_pos, snake_body, food_pos, food_spawn, direction
+    direction = [120,60]
+    snake_body = [[120,60]]
+    food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size,
+                random.randrange(1,(frame_size_y // square_size)) * square_size]
