2703b59d6510647b3572193c3a6fc08a8833d0f2
Karl Adrian <karladrian995@gmail.com>
2025-04-29 11:15:50 +0300

    add pygame, I forgot lol

--- snake.py
+++ snake.py
@@ -44,8 +44,16 @@ def init_vars():
     score = 0
 init_vars()
 
-def show_score():
-    print("showing score")
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render("Score : " + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (frame_size_x / 10, 15)
+    else:
+        score_rect.midtop = (frame_size_x / 2, frame_size_y / 1.25)
+    game_window.blit(score_surface, score_rect)
+
 
 #game loop
 
@@ -93,3 +101,16 @@ while True:
         if not food_spawn:
             food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size, random.randrange(1,(frame_size_y // square_size)) * square_size]
         #GFX
+        game_window.fill(black)
+        for pos in snake_body:
+            pygame.draw.rect(game_window, green, pygame.Rect(pos[0] + 2, pos[1] + 2, square_size - 2, square_size))
+        pygame.draw.rect(game_window,red, pygame.Rect(food_pos[0], food_pos[1], square_size, square_size))
+
+        #game over condition
+        for block in snake_body[1:]:
+            if head_pos[0] == block[0] and head_pos[1] == block[1]:
+                init_vars()
+
+        show_score(1,white, 'consolas', 20)
+        pygame.display.update()
+        fps_controller.tick(speed)
